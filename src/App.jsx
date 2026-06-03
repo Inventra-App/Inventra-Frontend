7c7a8d0e299a3809755436913ffb30e35a004102
Onyema512 <aonyema512@gmail.com>
2026-06-03 16:35:20 +0100

    Save current changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Pricing from './Pages/Auth/Pricing'
 import AboutUs from './Pages/Auth/AboutUs'
 import ContactUs from './Pages/Auth/ContactUs'
 import SignUp from './Pages/Auth/SignUp'
+import Login from './Pages/Auth/Login'
+import SignUpVerify from './Pages/Auth/SignUpVerify'
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
       <Route path='/about' element={<AboutUs />} />
       <Route path='/contact' element={<ContactUs />} />
       <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signupverify' element={<SignUpVerify />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/Pages/Auth/SupermarketInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Css/SupermarketInfo.css'
+
+const SupermarketInfo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SupermarketInfo
